2e3d107e1a71a445972d6c3f295ce0fbbc1c7e4b
Joe Hill <joe@jkh3.com>
2017-04-05 21:51:20 -0400

    Added new frames, added Start and Stop animating button, Added Slide In, Fade In, and Grow animation

--- animations/ViewController.swift
+++ animations/ViewController.swift
@@ -10,20 +10,82 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func fadeIn(_ sender: AnyObject) {
+        
+        image.alpha = 0
+        
+        UIView.animate(withDuration: 1.5, animations: {
+        
+            self.image.alpha = 1
+            
+        })
+    }
+    
+    @IBAction func slideIn(_ sender: AnyObject) {
+        
+        image.center = CGPoint(x: image.center.x - 500, y: image.center.y)
+        
+        UIView.animate(withDuration: 2) {
+            
+             self.image.center = CGPoint(x: self.image.center.x + 500, y: self.image.center.y)
+            
+        }
+        
+    }
+    
+    @IBAction func grow(_ sender: AnyObject) {
+        
+        image.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        
+        UIView.animate(withDuration: 1) {
+            self.image.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+            
+        }
+        
+        
+    }
+
+    
+    @IBOutlet var button: UIButton!
     @IBOutlet var image: UIImageView!
     
     var counter = 1
     
-    @IBAction func next(_ sender: AnyObject) {
+    var isAnimating = false
+    
+    var timer = Timer()
+    
+    func animate() {
         
-        image.image = UIImage(named: "frame_\(counter)_delay-0.04s.gif")
+        image.image = UIImage(named: "frame_\(counter)_delay-0.07s.gif")
         
         counter += 1
         
-        if counter == 33 {
+        if counter == 25 {
             
             counter = 0
             
+        }
+        
+    }
+    
+    @IBAction func next(_ sender: AnyObject) {
+        
+        if isAnimating {
+            
+            timer.invalidate()
+            button.setTitle("Start Animation", for: [])
+            isAnimating = false
+            
+        } else {
+        
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate), userInfo: nil, repeats: true)
+            
+            button.setTitle("Stop Animation", for: [])
+            
+            isAnimating = true
+        
         }
         
     }
